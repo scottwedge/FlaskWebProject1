cab07b7a1f1ffa0eb8a1c329946e1a1a3a5047a7
Daniar <dan.zh.1999@gmail.com>
2020-05-02 12:07:36 +0600

    second commit

--- FlaskWebProject1/FlaskWebProject1/datapresent.py
+++ FlaskWebProject1/FlaskWebProject1/datapresent.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
@@ -9,16 +10,32 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from mlxtend.plotting import plot_linear_regression
+
 from itertools import zip_longest
+from itertools import chain, repeat
+
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier
 
 class bankmarketing:
 
     bank = ''
-    accuracy = 0
     bank_all = dict()
+    data = ''
+    bank_kmeans = ''
+    cluster_centers = np.array([[-1.5,-1.5], [-1.6,-0.5], [-1.7,0.5], [-1.9,1.5], [-2,2.5],
+                                [0.5,-1], [0,0], [0,1], [-0.2,2], [-0.5, 2.8],
+                                [3,-1], [3,0], [2.5,1.1], [2.5,2], [2.5,3.2]])
 
     def __init__(self):  
          self.bank = pd.read_csv(r'C:\Users\Gintoki\source\repos\FlaskWebProject1\FlaskWebProject1\FlaskWebProject1\bank-additional-full.csv', sep = ';')
+         self.data = self.bank.iloc[: , 0:-1]
+         self.bank_kmeans = self.bank.iloc[: , 0:-1]
          
     def dataframe(self):
         return self.bank
@@ -72,8 +89,6 @@ class bankmarketing:
                              'contact', 'month', 'day_of_week', 'duration', 'emp.var.rate', 'cons.price.idx', 
                              'cons.conf.idx', 'euribor3m', 'nr.employed', 'campaign', 'pdays', 'previous', 'poutcome']]
         
-        #self.bank_all = dict(zip_longest(['bank_client', 'bank_related', 'bank_se, bank_o'], [bank_client, bank_related, bank_se, bank_o]))
-        from itertools import chain, repeat
         self.bank_all = dict(zip(['bank_client', 'bank_related', 'bank_se', 'bank_o'], chain([bank_client, bank_related, bank_se, bank_o], repeat(None))))
         return bank_final, y
        
@@ -92,13 +107,278 @@ class bankmarketing:
         return X_train, X_test, y_train, y_test
 
 
-    def logregression(self):
-        X_train, X_test, y_train, y_test = self.preprocessing()
-        logmodel = LogisticRegression() 
-        logmodel.fit(X_train,y_train)
-        logpred = logmodel.predict(X_test)
 
+    def plot_age_1(self):
+        fig, ax = plt.subplots()
+        fig.set_size_inches(20, 8)
+        sns.countplot(x = 'age', data = self.data)
+        ax.set_xlabel('Age', fontsize=15)
+        ax.set_ylabel('Count', fontsize=15)
+        ax.set_title('Age Count Distribution', fontsize=15)
+        sns.despine()
+        return fig
+
+    def plot_age_2(self):
+        fig, (ax1, ax2) = plt.subplots(nrows = 1, ncols = 2, figsize = (13, 5))
+        sns.boxplot(x = 'age', data = self.data, orient = 'v', ax = ax1)
+        ax1.set_xlabel('People Age', fontsize=15)
+        ax1.set_ylabel('Age', fontsize=15)
+        ax1.set_title('Age Distribution', fontsize=15)
+        ax1.tick_params(labelsize=15)
+
+        sns.distplot(self.data['age'], ax = ax2)
+        sns.despine(ax = ax2)
+        ax2.set_xlabel('Age', fontsize=15)
+        ax2.set_title('Age Distribution', fontsize=15)
+        ax2.tick_params(labelsize=15)
+
+        plt.subplots_adjust(wspace=0.5)
+        plt.tight_layout() 
+        return fig
+
+    def plot_jobs(self):
+     
+        fig, ax = plt.subplots()
+        fig.set_size_inches(20, 8)
+        sns.countplot(x = 'job', data = self.data)
+        ax.set_xlabel('Job', fontsize=15)
+        ax.set_ylabel('Count', fontsize=15)
+        ax.set_title('Age Count Distribution', fontsize=15)
+        ax.tick_params(labelsize=15)
+        sns.despine()
+        return fig
+
+    def plot_marital(self):
         
-        self.accuracy = round(accuracy_score(y_test, logpred),2)*100
-        #LOGCV = (cross_val_score(logmodel, X_train, y_train, cv=k_fold, n_jobs=1, scoring = 'accuracy').mean())
-        return self.accuracy
+        fig, ax = plt.subplots()
+        fig.set_size_inches(10, 5)
+        sns.countplot(x = 'marital', data = self.data)
+        ax.set_xlabel('Marital', fontsize=15)
+        ax.set_ylabel('Count', fontsize=15)
+        ax.set_title('Age Count Distribution', fontsize=15)
+        ax.tick_params(labelsize=15)
+        sns.despine()
+        return fig
+
+    def plot_education(self):
+        fig, ax = plt.subplots()
+        fig.set_size_inches(20, 5)
+        sns.countplot(x = 'education', data = self.data)
+        ax.set_xlabel('Education', fontsize=15)
+        ax.set_ylabel('Count', fontsize=15)
+        ax.set_title('Education Count Distribution', fontsize=15)
+        ax.tick_params(labelsize=15)
+        sns.despine()
+        return fig
+
+    def plot_DHL(self):
+        fig, (ax1, ax2, ax3) = plt.subplots(nrows = 1, ncols = 3, figsize = (20,8))
+        sns.countplot(x = 'default', data = self.data, ax = ax1, order = ['no', 'unknown', 'yes'])
+        ax1.set_title('Default', fontsize=15)
+        ax1.set_xlabel('')
+        ax1.set_ylabel('Count', fontsize=15)
+        ax1.tick_params(labelsize=15)
+
+        sns.countplot(x = 'housing', data = self.data, ax = ax2, order = ['no', 'unknown', 'yes'])
+        ax2.set_title('Housing', fontsize=15)
+        ax2.set_xlabel('')
+        ax2.set_ylabel('Count', fontsize=15)
+        ax2.tick_params(labelsize=15)
+
+        sns.countplot(x = 'loan', data = self.data, ax = ax3, order = ['no', 'unknown', 'yes'])
+        ax3.set_title('Loan', fontsize=15)
+        ax3.set_xlabel('')
+        ax3.set_ylabel('Count', fontsize=15)
+        ax3.tick_params(labelsize=15)
+
+        plt.subplots_adjust(wspace=0.25)
+        return fig
+
+    def plot_duration(self):
+        fig, (ax1, ax2) = plt.subplots(nrows = 1, ncols = 2, figsize = (13, 5))
+        sns.boxplot(x = 'duration', data = self.data, orient = 'v', ax = ax1)
+        ax1.set_xlabel('Calls', fontsize=10)
+        ax1.set_ylabel('Duration', fontsize=10)
+        ax1.set_title('Calls Distribution', fontsize=10)
+        ax1.tick_params(labelsize=10)
+
+        sns.distplot(self.data['duration'], ax = ax2)
+        sns.despine(ax = ax2)
+        ax2.set_xlabel('Duration Calls', fontsize=10)
+        ax2.set_title('Duration Distribution', fontsize=10)
+        ax2.tick_params(labelsize=10)
+
+        plt.subplots_adjust(wspace=0.5)
+        plt.tight_layout() 
+
+        return fig
+
+    def plot_con_day_month(self):
+        fig, (ax1, ax2, ax3) = plt.subplots(nrows = 1, ncols = 3, figsize = (15,6))
+        sns.countplot(self.data['contact'], ax = ax1)
+        ax1.set_xlabel('Contact', fontsize = 10)
+        ax1.set_ylabel('Count', fontsize = 10)
+        ax1.set_title('Contact Counts')
+        ax1.tick_params(labelsize=10)
+
+        sns.countplot(self.data['month'], ax = ax2, order = ['mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'])
+        ax2.set_xlabel('Months', fontsize = 10)
+        ax2.set_ylabel('')
+        ax2.set_title('Months Counts')
+        ax2.tick_params(labelsize=10)
+
+        sns.countplot(self.data['day_of_week'], ax = ax3)
+        ax3.set_xlabel('Day of Week', fontsize = 10)
+        ax3.set_ylabel('')
+        ax3.set_title('Day of Week Counts')
+        ax3.tick_params(labelsize=10)
+
+        plt.subplots_adjust(wspace=0.25)
+
+        return fig
+
+    def linear(self):
+        X_train, X_test, y_train, y_test = self.preprocessing()
+
+        linreg = LinearRegression(normalize=True) 
+        linreg.fit(X_train,y_train)
+        linpred = linreg.predict(X_test)
+
+
+        r2 = round(metrics.r2_score(y_test,linpred), 2)
+        mae = round(metrics.mean_absolute_error(y_test,linpred),2)
+        mse = round(metrics.mean_squared_error(y_test,linpred),2)
+        coef = linreg.coef_
+        return r2, mae, mse, coef, linpred
+        
+
+    def plot_linear(self):
+        bank_final, y = self.encode()
+        bank_final = bank_final[:, np.newaxis, 2]
+        X_train, X_test, y_train, y_test = train_test_split(bank_final, y, test_size = 0.5, random_state = 101)
+
+        r2, mae, mse, coef, linpred = self.linear()
+        fig, ax = plt.subplots()
+        
+        ax.scatter(X_test, y_test,  color='black')
+        ax.plot(X_test, linpred, color='blue', linewidth=3)
+
+        ax.xticks(())
+        ax.yticks(())
+        return fig
+
+
+    def kmeans(self):
+        self.bank_kmeans['campaign'] = self.bank_kmeans['campaign'].apply(lambda x: x if x<5 else 5) 
+        def treat_pdays(value):
+            if value <= 10:
+                return 1
+            if value > 10 and value <= 27:
+                return 2
+            if value > 27:
+                return 0
+
+        self.bank_kmeans['pdays'] = self.bank_kmeans['pdays'].apply(treat_pdays)
+
+        def treat_previous(value):
+    
+            if value == 0:
+                return 0
+            if value == 1:
+                return 1
+            else:
+                return 2
+    
+        self.bank_kmeans['previous'] = self.bank_kmeans['previous'].apply(treat_previous)
+
+
+        catg_cols = ['job', 'marital', 'education', 'default', 'housing',
+             'loan', 'contact', 'month', 'day_of_week', 'campaign', 'pdays', 'previous',
+             'poutcome']
+
+        num_cols = ['age', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx',
+                    'euribor3m', 'nr.employed']
+
+        dummy_features = pd.get_dummies(self.bank_kmeans[catg_cols])
+
+        num_features = self.bank_kmeans[num_cols]
+
+        scaler = StandardScaler()
+
+        num_features = pd.DataFrame(scaler.fit_transform(num_features), columns=num_features.columns)
+
+        preprocessed_df = pd.concat([dummy_features, num_features], axis=1)
+
+        labels = self.bank['y'].map({'no':0, 'yes':1})
+
+        pca = PCA(n_components=2)
+        pcs = pca.fit_transform(preprocessed_df)
+
+        pcs_df = pd.DataFrame(pcs)
+
+        n_clusters = 15 
+
+        kmeans = KMeans(n_clusters=15, max_iter=10000, verbose=1, init=self.cluster_centers)
+
+        clusters = kmeans.fit_predict(pcs_df)
+
+        pcs_df['cluster'] = clusters
+
+        return pcs, pcs_df, labels
+
+    def plot_kmeans_1(self):
+        pcs, pcs_df, labels = self.kmeans()
+        def plot_2d(X, y):   
+            fig, ax = plt.subplots()
+            for l in zip(np.unique(y)):
+                ax.scatter(X[y==l, 0], X[y==l, 1], label=l)
+            ax.legend(loc='upper right')
+            return fig
+        
+        return plot_2d(pcs, labels)
+
+
+    def plot_kmeans_2(self):
+        pcs, pcs_df, labels = self.kmeans()
+        fig, ax = plt.subplots()
+        ax.scatter(pcs_df[0], pcs_df[1], c=pcs_df['cluster'])
+        
+        return fig
+
+    def knn(self):
+        X_train, X_test, y_train, y_test = self.preprocessing()
+
+        knn = KNeighborsClassifier(n_neighbors=22)
+        knn.fit(X_train, y_train)
+        knnpred = knn.predict(X_test)
+
+        conf = confusion_matrix(y_test, knnpred)
+        accuracy = accuracy_score(y_test, knnpred)*100
+
+        return conf, accuracy
+
+    def plot_knn_conf(self):
+        conf, accuracy = self.knn()
+        fig, ax = plt.subplots()
+        sns.heatmap(conf, annot=True, ax=ax)
+        return fig
+
+
+    def xgboost(self):
+        bank_final, y = self.encode()
+        X_train, X_test, y_train, y_test = train_test_split(bank_final, y, test_size = 0.1942313295, random_state = 101)
+
+        xgb = XGBClassifier()
+        xgb.fit(X_train, y_train)
+        xgbprd = xgb.predict(X_test)
+
+        conf = confusion_matrix(y_test, xgbprd )
+        accuracy = accuracy_score(y_test, xgbprd)*100
+
+        return conf, accuracy
+
+    def plot_xgboost_conf(self):
+        conf, accuracy = self.xgboost()
+        fig, ax = plt.subplots()
+        sns.heatmap(conf, annot=True, ax=ax)
+        return fig
--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -18,7 +18,7 @@ import seaborn  as sns
 
 banks = bankmarketing()
 
-@app.route('/')
+@app.route('/1')
 def home():
 
     Markdown(app)
@@ -32,121 +32,182 @@ def home():
 
 @app.route('/analysis')
 def analysis():
-    data = banks.bank.iloc[: , 0:7]
-    age_min = data['age'].min()
-    age_max = data['age'].max()
+    data = banks.bank.iloc[: , 0:-1]
+    table = banks.bank.iloc[: , 0:7]
+    table_related = banks.bank.iloc[: , 7:11]
+    other_attributes = banks.bank.loc[: , ['emp.var.rate', 'cons.price.idx',
+                                          'cons.conf.idx', 'euribor3m', 'nr.employed',
+                                          'campaign', 'pdays','previous', 'poutcome']]
 
-    quart1 = data['age'].quantile(q = 0.25)
-    quart2 = data['age'].quantile(q = 0.50)
-    quart3 = data['age'].quantile(q = 0.75)
-    quart4 = data['age'].quantile(q = 1.00)
+    age = list_creator(data['age'].min(), data['age'].max(), round(data['age'].std()), round(data['age'].mean()))
+
     outliners = data['age'].quantile(q = 0.75) + 1.5*(data['age'].quantile(q = 0.75) - data['age'].quantile(q = 0.25))
-    age_mean = round(data['age'].mean())
-    age_std = round(data['age'].std())
+    quart = list_creator(data['age'].quantile(q = 0.25), data['age'].quantile(q = 0.50),
+                        data['age'].quantile(q = 0.75), data['age'].quantile(q = 1.00), outliners)
 
-    Markdown(app)
+    unique = list_creator(data['job'].unique(), data['marital'].unique(), data['education'].unique(),
+                          data['default'].unique(), data['housing'].unique(), data['loan'].unique())
+
+    unique_related = list_creator(data['contact'].unique(), data['month'].unique(),
+                                 data['day_of_week'].unique()) 
+
+    duration_call = list_creator(round(data['duration'].min()/60), round(data['duration'].max()/60),
+                                 round(data['duration'].std()/60), round(data['duration'].mean()/60))
+    quart_duration = list_creator(data['duration'].quantile(q = 0.25), data['duration'].quantile(q = 0.50),
+                                  data['duration'].quantile(q = 0.75), data['duration'].quantile(q = 1.00))
+
     return render_template(
         'analysis.html',
          year=datetime.now().year,
-         tables=[data.head().to_html(classes='table table-bordered')],
-         jobs=data['job'].unique(),
-         marital=data['marital'].unique(),
-         education=data['education'].unique(),
-         default=data['default'].unique(),
-         housing=data['housing'].unique(),
-         loan=data['loan'].unique(),
-         age_min=age_min,
-         age_max=age_max,
-         age_mean=age_mean,
-         age_std=age_std,
-         quart1=quart1,
-         quart2=quart2,
-         quart3=quart3,
-         quart4=quart4
+         tables=[table.head().to_html(classes='table table-bordered')],
+         table_related=[table_related.head().to_html(classes='table table-bordered')],
+         unique=unique,
+         age=age,
+         quart=quart,
+         unique_related=unique_related,
+         duration_call=duration_call,
+         quart_duration=quart_duration,
+         others=[other_attributes.head().to_html(classes='table table-bordered')],
          )
 
-@app.route('/linear')
+@app.route('/')
 def linear():
-    accuracy = {'logres': banks.logregression()}
-    Markdown(app)
+    r2, mae, mse, coef, linpred = banks.linear()
 
     return render_template(
         'linear.html',
          year=datetime.now().year,
+         r2=r2,
+         mae=mae,
+         mse=mse,
+         coef=coef
          )
 
 @app.route('/xgboost')
 def xgboost():
-
-    Markdown(app)
+    conf_xg, accuracy_xg = banks.xgboost()
+    accuracy_xg = (float(int(accuracy_xg*1000)))/1000
 
     return render_template(
         'xgboost.html',
          year=datetime.now().year,
+         conf_xg=conf_xg,
+         accuracy_xg=accuracy_xg
          )
 
 @app.route('/kmeans')
 def kmeans():
-
-    Markdown(app)
-
+    centers = banks.cluster_centers
+    
     return render_template(
         'kmeans.html',
          year=datetime.now().year,
+         centers=centers,
          )
 
 @app.route('/knn')
 def knn():
-
-    Markdown(app)
-
+    conf, accuracy = banks.knn()
+    accuracy = (float(int(accuracy*1000)))/1000
     return render_template(
         'knn.html',
          year=datetime.now().year,
+         conf=conf,
+         accuracy=accuracy
          )
 
 
 @app.route('/plot_age1.png')
 def plot_age1():
-    fig = create_figure1()
+    fig = banks.plot_age_1()
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure1():
-    data = banks.bank.iloc[: , 0:7]
-    fig, ax = plt.subplots()
-    fig.set_size_inches(20, 8)
-    sns.countplot(x = 'age', data = data)
-    ax.set_xlabel('Age', fontsize=15)
-    ax.set_ylabel('Count', fontsize=15)
-    ax.set_title('Age Count Distribution', fontsize=15)
-    sns.despine()
-    return fig
 
 @app.route('/plot_age2.png')
 def plot_age2():
-    fig = create_figure2()
+    fig = banks.plot_age_2()
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-def create_figure2():
-    data = banks.bank.iloc[: , 0:7]
-    fig, (ax1, ax2) = plt.subplots(nrows = 1, ncols = 2, figsize = (13, 5))
-    sns.boxplot(x = 'age', data = data, orient = 'v', ax = ax1)
-    ax1.set_xlabel('People Age', fontsize=15)
-    ax1.set_ylabel('Age', fontsize=15)
-    ax1.set_title('Age Distribution', fontsize=15)
-    ax1.tick_params(labelsize=15)
+@app.route('/plot_jobs.png')
+def plot_jobs():
+    fig = banks.plot_jobs()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-    sns.distplot(data['age'], ax = ax2)
-    sns.despine(ax = ax2)
-    ax2.set_xlabel('Age', fontsize=15)
-    ax2.set_ylabel('Occurence', fontsize=15)
-    ax2.set_title('Age x Ocucurence', fontsize=15)
-    ax2.tick_params(labelsize=15)
+@app.route('/plot_marital.png')
+def plot_marital():
+    fig = banks.plot_marital()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-    plt.subplots_adjust(wspace=0.5)
-    plt.tight_layout() 
-    return fig
+@app.route('/plot_education.png')
+def plot_education():
+    fig = banks.plot_education()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_DHL.png')
+def plot_DHL():
+    fig = banks.plot_DHL()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_duration.png')
+def plot_duration():
+    fig = banks.plot_duration()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_con_day_month.png')
+def plot_con_day_month():
+    fig = banks.plot_con_day_month()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+@app.route('/plot_kmeans_1.png')
+def plot_kmeans_1():
+    fig = banks.plot_kmeans_1()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_kmeans_2.png')
+def plot_kmeans_2():
+    fig = banks.plot_kmeans_2()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_knn_conf.png')
+def plot_knn_conf():
+    fig = banks.plot_knn_conf()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_xgboost_conf.png')
+def plot_xgboost_conf():
+    fig = banks.plot_xgboost_conf()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plot_linear.png')
+def plot_linear():
+    fig = banks.plot_linear()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def list_creator(*args):
+    return list(args)
